3ca07696f070e866a486ce54aff1841b0fe691d2
ogosborne <47107226+ogosborne@users.noreply.github.com>
2024-12-20 15:27:11 +0000

    Create PASA_Update_Annots.sh

---
+++ 09_Annotation_consolidation/PASA_Update_Annots.sh
@@ -0,0 +1,42 @@
+
+### after EVM, rerun PASA to add UTR and alternatively spliced transcripts
+
+# Load EVM gene annotations
+$PASAHOME/scripts/Load_Current_Gene_Annotations.dbi \
+     -c sqlite.alignAssembly.config \
+     -g Su_softmasked.fasta \
+     -P all.evm.gff3 |& tee Load_Current_Gene_Annotations1.log
+
+# Update EVM annotations
+$PASAHOME/Launch_PASA_pipeline.pl \
+        -c annotCompare.config \
+        -A \
+        -g Su_softmasked.fasta \
+        -t Trinity.fasta.clean |& tee pasa.annotCompare1.log
+# Trinity.fasta.clean created in 07_RNAseq_alignmen/Trinity_PASA.sh
+
+# load new PASA annotations (1)
+$PASAHOME/scripts/Load_Current_Gene_Annotations.dbi \
+     -c sqlite.alignAssembly.config \
+     -g Su_softmasked.fasta \
+     -P mydb_pasa.sqlite.gene_structures_post_PASA_updates.27063.gff3 |& tee Load_Current_Gene_Annotations2.log
+
+# Update PASA annotations (1)
+$PASAHOME/Launch_PASA_pipeline.pl \
+        -c annotCompare.config \
+        -A \
+        -g Su_softmasked.fasta \
+        -t Trinity.fasta.clean |& tee pasa.annotCompare2.log
+
+# load new PASA annotations (2)
+$PASAHOME/scripts/Load_Current_Gene_Annotations.dbi \
+     -c /sqlite.alignAssembly.config \
+     -g Su_softmasked.fasta \
+     -P mydb_pasa.sqlite.gene_structures_post_PASA_updates.34512.gff3 |& tee Load_Current_Gene_Annotations3.log
+
+# Update PASA annotations (2)
+$PASAHOME/Launch_PASA_pipeline.pl \
+        -c annotCompare.config \
+        -A \
+        -g Su_softmasked.fasta \
+        -t Trinity.fasta.clean |& tee pasa.annotCompare3.log
