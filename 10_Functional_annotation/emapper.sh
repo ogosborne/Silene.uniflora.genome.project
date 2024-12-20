18910648bb80c60f5af028a29874c0aec9ac5b3e
ogosborne <47107226+ogosborne@users.noreply.github.com>
2024-12-20 15:36:31 +0000

    Create emapper.sh

---
+++ 10_Functional_annotation/emapper.sh
@@ -0,0 +1,14 @@
+emapper.py \
+--tax_scope 33090 \
+--output_dir results \
+--data_dir data \
+--output eggNOG_mapper \
+--query_cover 70 \
+--subject_cover 70 \
+--evalue 0.0001 \
+-m diamond \
+--pfam_realign realign \
+--cpu 20 \
+--num_servers 20 \
+-i PASA.final.gff3.pep.fasta | tee emapper.log
+
