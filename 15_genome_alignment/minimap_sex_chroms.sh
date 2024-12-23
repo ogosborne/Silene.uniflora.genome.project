8f3ae94335458d30d5aebcafeaae0c041024f92f
ogosborne <47107226+ogosborne@users.noreply.github.com>
2024-12-23 10:03:11 +0000

    Create minimap_sex_chroms.sh

---
+++ 15_genome_alignment/minimap_sex_chroms.sh
@@ -0,0 +1,4 @@
+minimap2 -cx splice --secondary=no --cs Su_softmasked.fasta Martin_2019_So-Sp_transcriptome/Silene_otites_assembly.fa > results/martin.So.paf
+minimap2 -cx splice --secondary=no --cs Su_softmasked.fasta Martin_2019_So-Sp_transcriptome/Silene_pseudotites_assembly.fa  > results/martin.Sp.paf
+minimap2 -cx splice --secondary=no --cs Su_softmasked.fasta Filatov_2011_Sl_transcriptome/Cons833d_newerFilatov.fasta > results/filatov.Sl.paf
+minimap2 -cx splice --secondary=no --cs Su_softmasked.fasta Zemp_2015_Sl_Transcriptome/FMHP01.rn.fasta > results/zemp.Sl.paf
