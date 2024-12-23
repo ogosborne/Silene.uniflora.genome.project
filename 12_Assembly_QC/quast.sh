cc61e23fdb0b8d50a528615fda517374e8b681cd
ogosborne <47107226+ogosborne@users.noreply.github.com>
2024-12-23 08:41:18 +0000

    Create quast.sh

---
+++ 12_Assembly_QC/quast.sh
@@ -0,0 +1,8 @@
+python quast-lg.py -o results \
+-l MaSuRCA,Chicago,Hi-C,SLR,TGSGC \
+-t 20 \
+SU_FPB30k99_v1.fasta \
+silene_uniflora_26Mar2019_ppToT_Chicago.fasta \
+Dovetail_v1.eq_mod.fa \
+SLR_scaffold_set.fa \
+TGS_GC.scaff_seqs.fa 
